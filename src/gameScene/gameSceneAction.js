8e76a71d595414876fd663984173c4da7f2d315b
SAWADA Takayoshi <sawadasuiren@gmail.com>
2018-03-25 16:39:08 +0900

    Add gameSceneAction.js

---
+++ src/gameScene/gameSceneAction.js
@@ -0,0 +1,19 @@
+export const SET_ON_LEFT = 'SET_ON_LEFT';
+export const setOnLeft = () => ({
+  type: SET_ON_LEFT,
+});
+
+export const SET_OFF_LEFT = 'SET_OFF_LEFT';
+export const setOffLeft = () => ({
+  type: SET_OFF_LEFT,
+});
+
+export const SET_ON_RIGHT = 'SET_ON_RIGHT';
+export const setOnRight = () => ({
+  type: SET_ON_RIGHT,
+});
+
+export const SET_OFF_RIGHT = 'SET_OFF_RIGHT';
+export const setOffRight = () => ({
+  type: SET_OFF_RIGHT,
+});
