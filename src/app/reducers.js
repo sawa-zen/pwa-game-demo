1d9f0a9f09dabdfe9d9fd3be47357a3c92d32317
SAWADA Takayoshi <sawadasuiren@gmail.com>
2018-03-25 16:59:01 +0900

    Add gameSceneReducer.js

--- src/app/reducers.js
+++ src/app/reducers.js
@@ -1,7 +1,10 @@
 import { combineReducers } from 'redux';
 import appReducer from './appReducer';
+import gameSceneReducer from '../gameScene/gameSceneReducer';
 
 const reducers = combineReducers({
   app: appReducer,
+  gameScene: gameSceneReducer,
 });
+
 export default reducers;
---
+++ src/gameScene/gameSceneReducer.js
@@ -0,0 +1,5 @@
+const gameSceneReducer = (state = {}, action) => {
+  return state;
+};
+
+export default gameSceneReducer;
