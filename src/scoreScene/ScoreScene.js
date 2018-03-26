634f140c9c1aac3bd0586a2557cee8ad88857c79
SAWADA Takayoshi <sawadasuiren@gmail.com>
2018-03-26 16:43:20 +0900

    Use pure in scoreScene

--- src/scoreScene/ScoreScene.js
+++ src/scoreScene/ScoreScene.js
@@ -1,14 +1,15 @@
 import React from 'react';
+import { pure } from 'recompose';
 import styled from 'styled-components';
 
 const Wrapper = styled.div`
   background: black;
 `;
 
-const ScoreScene = () => (
+const ScoreScene = pure(() => (
   <Wrapper>
     ScoreScene
   </Wrapper>
-);
+));
 
 export default ScoreScene;
