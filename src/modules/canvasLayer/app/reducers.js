a3305b8a462721f95bf442328fdb763a82842e5d
SAWADA Takayoshi <sawadasuiren@gmail.com>
2018-03-26 18:35:05 +0900

    Add appReducer.js

--- src/modules/canvasLayer/app/reducers.js
+++ src/modules/canvasLayer/app/reducers.js
@@ -1,8 +1,10 @@
 import { combineReducers } from 'redux';
+import appReducer from './appReducer';
 import meteorEmitterReducer from '../meteorEmitter/meteorEmitterReducer';
 import playerReducer from '../player/playerReducer';
 
 const reducers = combineReducers({
+  app: appReducer,
   meteorEmitter: meteorEmitterReducer,
   player: playerReducer,
 });
