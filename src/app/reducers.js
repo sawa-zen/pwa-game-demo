37d657f8ad9e0a887d8dd9eb2c6dc42a125ee4ba
SAWADA Takayoshi <sawadasuiren@gmail.com>
2018-03-30 23:23:57 +0900

    Fix

--- src/app/reducers.js
+++ src/app/reducers.js
@@ -1,10 +1,8 @@
 import { combineReducers } from 'redux';
 import appReducer from './appReducer';
-import gameSceneReducer from '../gameScene/gameSceneReducer';
 
 const reducers = combineReducers({
   app: appReducer,
-  gameScene: gameSceneReducer,
 });
 
 export default reducers;
