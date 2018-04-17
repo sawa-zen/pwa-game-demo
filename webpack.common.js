a931ec4afbfd27ed61bc8802e7fcc002dd7e9665
SAWADA Takayoshi <sawadasuiren@gmail.com>
2018-04-18 02:06:23 +0900

    Remove unused package

--- webpack.common.js
+++ webpack.common.js
@@ -1,6 +1,5 @@
 const path = require('path');
 const webpack = require('webpack');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
 const projectRootPath = path.resolve(__dirname, './');
 const assetsPath = path.resolve(projectRootPath, './public');
 
