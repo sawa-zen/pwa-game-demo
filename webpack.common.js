658b5e40980dff87076c2343298bf1c3f406126c
SAWADA Takayoshi <sawadasuiren@gmail.com>
2018-04-18 01:42:38 +0900

    Fix webpack settings

--- webpack.common.js
+++ webpack.common.js
@@ -1,47 +1,26 @@
 const path = require('path');
 const webpack = require('webpack');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-const CleanWebpackPlugin = require('clean-webpack-plugin');
-
 const projectRootPath = path.resolve(__dirname, './');
-const assetsPath = path.resolve(projectRootPath, './dist');
+const assetsPath = path.resolve(projectRootPath, './public');
 
 module.exports = {
-  entry: {
-    index: `${__dirname}/src/index.js`,
-  },
-  plugins: [
-    new CleanWebpackPlugin([assetsPath], { root: projectRootPath }),
-    new HtmlWebpackPlugin({
-      hash: true,
-      title: 'METEOR',
-      filename: 'index.html',
-      template: './src/template.html',
-      chunks: ['index'],
-    }),
-    new webpack.NamedModulesPlugin(),
-    new webpack.NoEmitOnErrorsPlugin(),
-  ],
+  entry: `${__dirname}/src/index.js`,
   output: {
     path: assetsPath,
     publicPath: '/',
+    filename: 'pwa-game-demo.js'
   },
   module: {
     rules: [
       {
-        test: /\.js[x]?$/,
+        test: /\.js?$/,
         loader: 'babel-loader',
         options: {
           compact: false,
           cacheDirectory: true,
         },
         exclude: /node_modules/,
-      },
-      {
-        test: /\.(png|svg|jpg|gif)$/,
-        use: [
-          'url-loader',
-        ],
       },
     ],
   },
